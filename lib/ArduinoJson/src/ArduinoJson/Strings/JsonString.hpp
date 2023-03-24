7ec4772a62218982934954a5299784f8af422abb
MichaelDvP <github@mdvp.de>
2023-03-24 19:04:00 +0100

    update arduionJson

--- lib/ArduinoJson/src/ArduinoJson/Strings/JsonString.hpp
+++ lib/ArduinoJson/src/ArduinoJson/Strings/JsonString.hpp
@@ -1,51 +1,57 @@
 // ArduinoJson - https://arduinojson.org
-// Copyright © 2014-2022, Benoit BLANCHON
+// Copyright © 2014-2023, Benoit BLANCHON
 // MIT License
 
 #pragma once
-
-#include <ArduinoJson/Misc/SafeBoolIdiom.hpp>
 
 #if ARDUINOJSON_ENABLE_STD_STREAM
 #  include <ostream>
 #endif
 
-namespace ARDUINOJSON_NAMESPACE {
+ARDUINOJSON_BEGIN_PUBLIC_NAMESPACE
 
-class String : public SafeBoolIdom<String> {
+// A string.
+// https://arduinojson.org/v6/api/jsonstring/
+class JsonString {
  public:
   enum Ownership { Copied, Linked };
 
-  String() : _data(0), _size(0), _ownership(Linked) {}
+  JsonString() : _data(0), _size(0), _ownership(Linked) {}
 
-  String(const char* data, Ownership ownership = Linked)
+  JsonString(const char* data, Ownership ownership = Linked)
       : _data(data), _size(data ? ::strlen(data) : 0), _ownership(ownership) {}
 
-  String(const char* data, size_t sz, Ownership ownership = Linked)
+  JsonString(const char* data, size_t sz, Ownership ownership = Linked)
       : _data(data), _size(sz), _ownership(ownership) {}
 
+  // Returns a pointer to the characters.
   const char* c_str() const {
     return _data;
   }
 
+  // Returns true if the string is null.
   bool isNull() const {
     return !_data;
   }
 
+  // Returns true if the string is stored by address.
+  // Returns false if the string is stored by copy.
   bool isLinked() const {
     return _ownership == Linked;
   }
 
+  // Returns length of the string.
   size_t size() const {
     return _size;
   }
 
-  // safe bool idiom
-  operator bool_type() const {
-    return _data ? safe_true() : safe_false();
+  // Returns true if the string is non-null
+  explicit operator bool() const {
+    return _data != 0;
   }
 
-  friend bool operator==(String lhs, String rhs) {
+  // Returns true if strings are equal.
+  friend bool operator==(JsonString lhs, JsonString rhs) {
     if (lhs._size != rhs._size)
       return false;
     if (lhs._data == rhs._data)
@@ -57,12 +63,13 @@ class String : public SafeBoolIdom<String> {
     return memcmp(lhs._data, rhs._data, lhs._size) == 0;
   }
 
-  friend bool operator!=(String lhs, String rhs) {
+  // Returns true if strings differs.
+  friend bool operator!=(JsonString lhs, JsonString rhs) {
     return !(lhs == rhs);
   }
 
 #if ARDUINOJSON_ENABLE_STD_STREAM
-  friend std::ostream& operator<<(std::ostream& lhs, const String& rhs) {
+  friend std::ostream& operator<<(std::ostream& lhs, const JsonString& rhs) {
     lhs.write(rhs.c_str(), static_cast<std::streamsize>(rhs.size()));
     return lhs;
   }
@@ -74,4 +81,4 @@ class String : public SafeBoolIdom<String> {
   Ownership _ownership;
 };
 
-}  // namespace ARDUINOJSON_NAMESPACE
+ARDUINOJSON_END_PUBLIC_NAMESPACE
